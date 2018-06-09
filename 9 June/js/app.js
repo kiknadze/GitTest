02b314fb98037caa24e9f19855155f11758b67b6
Temo Kiknadze <temo.kiknadze@yahoo.com>
2018-06-09 16:51:54 +0400

    9 ივნისი

---
+++ 9 June/js/app.js
@@ -0,0 +1,63 @@
+//debuger;
+//alert("hellow world");
+//confirm("are");
+//promt("enter");
+
+//ASI
+
+// alert("1")
+// alert("2")
+
+/*
+    multiline comment
+*/
+
+//var, let, const
+// const COLOR_GREEN = '#0f0';
+// const COLOR_ORANGE = '#ff7f00';
+
+// let message;
+// message = "my message";
+
+// console.log(message);
+
+// let user = 'John', age = 33;
+// console.log(message, user, age);
+
+//camelCase
+
+// let name = 'John';
+// let admin = name;
+
+// alert(admin);
+
+
+// let foo = 99;
+// console.log(typeof foo);
+// foo = "bar";
+// console.log(typeof foo);
+// foo = true;
+// console.log(typeof foo);
+// foo = null;
+// console.log(typeof foo);
+// foo = undefined;
+// console.log(typeof foo);
+// console.log(1/0);
+// console.log(Infinity);
+// console.log("number"*2); // isNaN()
+// //null
+
+// let age = 13;
+// let message = 'sheni asakia: ' + age;
+// console.log(message);
+// message = `sheni asakia: ${age}`;
+// console.log(message);
+
+// let checked = false;
+// let isValid = true;
+
+// let isGreater = 5 > 6;
+
+typeof 0;
+typeof Math;
+typeof alert;
