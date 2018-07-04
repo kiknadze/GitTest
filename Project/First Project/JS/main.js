3950a624b5cfbe1d290d00e35a653504157cff32
Temo <temo.kiknadze@yahoo.com>
2018-07-05 00:08:42 +0400

    5 ივლისი

---
+++ Project/First Project/JS/main.js
@@ -0,0 +1,24 @@
+const studNumb = 15;
+
+function addDay() {
+    let parent = document.querySelector('.table-body');
+    let grade = document.createElement('div');
+    grade.setAttribute('class', 'grade');
+    parent.appendChild(grade);
+
+    for(let i=0; i<=studNumb; i++) {
+        let myDiv = document.createElement('div');
+        myDiv.setAttribute('class', 'box');
+        if(i==0) {
+            myDiv.appendChild(document.createTextNode('date'));
+        } else {
+            myDiv.appendChild(document.createTextNode('0'));
+        }
+        grade.appendChild(myDiv);
+    }
+}
+
+function removeDay() {
+    let box = document.querySelector('.grade:last-child');
+    box.parentNode.removeChild(box);
+}
