b140cce5f5b7786b673a282f48b35d496194a908
Temo <temo.kiknadze@yahoo.com>
2018-08-29 17:08:23 +0400

    დავალება FETCH

---
+++ Excersices/fetch.js
@@ -0,0 +1,39 @@
+if (localStorage.getItem("User0")) {
+    console.log('მომხმარებლები უკვე არსებობს')
+} else {
+        $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/users',
+        data: null,
+        method: 'get',
+        dataType: 'json',
+        success(data, status, xhr) {
+            let i=0;
+            while(data) {
+                if(data[i] == undefined) break;
+                localStorage.setItem(`User${i}`, `Name: ${data[i].name}, E-mail: ${data[i].email}`);
+                i++;
+            }
+            printUsers()
+        },
+        error(xhr, status, error){
+            console.log(error);
+        }
+    })
+}
+
+function printUsers(){
+    $(myDiv).empty();
+    for (var i = 0; i < localStorage.length; i++){
+        $(myDiv).append(`<div>${localStorage.getItem(`User${i}`)}</div>`);
+    }
+}
+
+
+$(myDiv).on('click', function(e){
+    let newValue = prompt('დაარედაქტირე ან წაშალე', localStorage.getItem(`User${($('div').index(e.target))-1}`));
+    if(newValue == '') localStorage.removeItem(`User${($('div').index(e.target))-1}`)
+    if(newValue != null) {
+        localStorage.setItem(`User${($('div').index(e.target))-1}`, newValue);
+        printUsers();
+    }
+})
