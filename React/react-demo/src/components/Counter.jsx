7b189781ef0d32d8306ad61cdbe0f524766a4b80
TEMO <temo.kiknadze@yahoo.com>
2018-11-14 15:42:50 +0400

    updated traveller

---
+++ React/react-demo/src/components/Counter.jsx
@@ -0,0 +1,11 @@
+import React, { Components } from 'react';
+
+class Counter extends Components {
+    render(){
+        return(
+            <div>
+                <h1>Count: </h1>
+            </div>
+        )
+    }
+}
