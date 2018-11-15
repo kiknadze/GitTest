aaa3d71d92c576f4f67250e4c88c1c55388f6592
TEMO <temo.kiknadze@yahoo.com>
2018-11-15 21:45:27 +0400

    update

--- React/react-demo/src/App.js
+++ React/react-demo/src/App.js
@@ -1,62 +1,116 @@
 import React, { Component } from 'react';
 import './App.css';
+import Counter from './components/Counter';
+import VisibilityToggle from './components/VisibilityToggle';
 
-class Options extends Component {
-  constructor(props){
-    super(props);
-    this.handleRemoveAll = this.handleRemoveAll.bind(this);
-  }
-  
-  handleRemoveAll(){
-    console.log(this.props.options)
-  }
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started</p>}
+      {
+        props.options.map((option) => (
+        <Option 
+          key={option} 
+          optionText={option} 
+          handleDeleteOption={props.handleDeleteOption}
+        />
+        ))
+      }
+    </div>
+  );
+};
 
-  render(){
-    return (
-      <div>
-        <button onClick={this.handleRemoveAll}>Remove All</button>
-        {
-          this.props.options.map((option) => <Option key={option} optionText={option} />)
-        }
-      </div>
-    )
-  }
-}
+// class Options extends Component {
 
-class Option extends Component {
-  render(){
-    return (
-      <div>
-        {this.props.optionText}
-      </div>
-    )
-  }
-}
+//   render(){
+//     return (
+//       <div>
+//         <button onClick={this.props.handleDeleteOptions}>Remove All</button>
+//         {
+//           this.props.options.map((option) => <Option key={option} optionText={option} />)
+//         }
+//       </div>
+//     )
+//   }
+// }
 
-class Action extends Component {
-  handlePick() {
-    alert('s')
-  }
+const Option = (props) => {
+  return (
+    <div>
+      {props.optionText}
+      <button 
+        onClick={(e) => {
+          props.handleDeleteOption(props.optionText);
+        }}
+      >
+        remove
+      </button>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.handlePick}>What Should I do?</button>
-      </div>
-    )
-  }
-}
+// class Option extends Component {
+//   render(){
+//     return (
+//       <div>
+//         {this.props.optionText}
+//       </div>
+//     )
+//   }
+// }
+
+const Action = (props) => {
+  return (
+    <div>
+      <button 
+        onClick={props.handlePick}
+        disabled={!props.hasOptions}
+      >
+      What Should I do?
+      </button>
+    </div>
+  );
+};
+
+// class Action extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <button 
+//           onClick={this.props.handlePick}
+//           disabled={!this.props.hasOptions}
+//         >
+//         What Should I do?
+//         </button>
+//       </div>
+//     )
+//   }
+// }
 
 class AddOption extends Component{
+  constructor(props){
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      error: undefined
+    };
+  }
   handleAddOption(e){
     e.preventDefault();
-    if(e.target.option.value){
-      alert(e.target.option.value)
+    const option = e.target.option.value.trim();
+    const error = this.props.handleAddOption(option);
+
+    this.setState( () => ( { error } ))
+
+    if(!error) {
+      e.target.elements.option.value ='';
     }
   }
   render(){
     return(
       <div>
+        {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.handleAddOption}>
           <input type='text' name='option' />
           <button>Submit</button>
@@ -66,17 +120,102 @@ class AddOption extends Component{
   }
 }
 
+const User = (props) => {
+  return(
+    <div>
+      <p>Name: {props.name} </p>
+      <p>age: {props.age} </p>
+    </div>
+  );
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
+    this.state = {
+      options: []
+    }
+  }
+
+  componentDidMount(){
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if(options) {
+        this.setState(() => ({ options }))
+      }
+    } catch (e) {
+
+    }
+    
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json)
+    }
+  }
+
+  componentWillUnmount(){
+    console.log('Component will unmount')
+  }
+
+  handleDeleteOptions(){
+    this.setState( () => ( { options: [] } ) );
+  }
+
+  handleDeleteOption(optionToRemove) {
+    this.setState((prevState) => ({
+      options: prevState.options.filter( (option) => optionToRemove !== option )
+    }))
+  }
+
+  handlePick(){
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option)
+  }
+
+  handleAddOption(option) {
+    if(!option) {
+      return 'Enter valid value to add item';
+    } else if(this.state.options.indexOf(option) > -1) {
+      return 'this option already exist';
+    } 
+
+    this.setState((prevState) => ( { options: prevState.options.concat(option) } ));
+  }
+
   render() {
-    const options = ['One Thing', 'Two Thing', 'Four Thing']
     return (
       <div className="App">
-        <Options options={options} />
-        <Action />
-        <AddOption />
+        <Options 
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <Action 
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <AddOption 
+          handleAddOption={this.handleAddOption}
+        />
+
+        <Counter />
+        <VisibilityToggle />
+
+        <User name='Temo' age='25'/>
       </div>
     );
   }
 }
+
 
 export default App;
